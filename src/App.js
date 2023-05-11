0bac19c3ee2a30d77a7ec7469edf221ff98a0621
Natalie Childs <nata.faye@gmail.com>
2023-05-10 22:06:55 -0700

    Products app with lots of random stuff shoved in

--- src/App.js
+++ src/App.js
@@ -1,110 +1,85 @@
 import React, { useEffect, useState } from 'react'
-import { deleteProduct } from './productService'
 
 export default function App() {
-	const [productList, setProductList] = useState([])
-	const [isExpanded, setIsExpanded] = useState(false)
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
-	useEffect(() => {
-		const refreshProducts = async () => {
-			const response = await fetch('http://localhost:3000/products')
-			const data = await response.json()
-			setProductList(data)
-		}
-		refreshProducts()
-	}, [])
+    useEffect(() => {
+        const getAllProducts = async () => {
+            setLoading(true)
+            const response = await fetch("http://localhost:3004/products")
+            const data = await response.json()
+            setLoading(false)
+            setProducts(data)
+        }
+        getAllProducts()
+    }, []) // run this little function once when this component first loads in, and never again
 
-	const onDelete = async (id) => {
-		await deleteProduct(id)
-		setProductList(productList.filter(p => p.id !== id))
-	}
+    
 
-	const createProduct = async (newProduct) => {
-		const response = await fetch("http://localhost:3000/products", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(newProduct)
-		})
-		const createdProduct = await response.json() // make sure you concat the product from the backend, because it has the id
-		setProductList(productList.concat(createdProduct))
-	}
+    const deleteProduct = async (id) => {
+        // Changing it on the backend
+        setLoading(true)
+        try {
+            const response = await fetch("http://localhost:3004/products/" + id, {
+                method: "DELETE"
+            })
+            setError(null)
+            // Changing it on the frontend
+            setProducts( products.filter(p => p.id !== id)  )
+        } catch(error) {
+            setError("There was an error!")
+        }
+        setLoading(false)
 
-	const updateProduct = async (updatedProduct) => {
-		fetch("http://localhost:3000/products" + updatedProduct.id, {
-			method: "PUT",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(updatedProduct)
-		})
-		setProductList(productList.map(p => p.id === updatedProduct.id ? updatedProduct : p))
-	}
+        
+    }
 
-	return (
-		<ul>
-			{ }
-			{productList.map(p => <li>{p.name} <button onClick={() => onDelete(p.id)}>Delete</button></li>)}
-		</ul>
-	)
+    const details = { something: 3, hello: 5 }
+
+    return (
+        <div>
+            App
+            { loading ? <p>Loading...</p> : null }
+            { error ? <p>{error}</p> : null }
+            <ProductList productList={products} anotherProp={details} handleDelete={deleteProduct} loading={loading} />
+        </div>
+    )
+}
+
+function ProductList({ productList, anotherProp, handleDelete, loading }) { // productList = [ { id: 0, title: "Shoes" } ]; anotherProp = { something: 3, hello: 5 }
+    return (
+        <ul>
+            { productList.map(p => <ProductItem key={p.id} product={p} onDelete={handleDelete} loading={loading}/>)}
+        </ul>
+    )
+}
+
+function ProductItem({ product, onDelete, loading }) {
+    return (
+        <li>
+            {product.name}
+            <button disabled={loading} onClick={() => onDelete(product.id)}>Delete</button>
+        </li>
+    )
 }
 
 
 
-// const bookList = [
-// 	{
-// 		id: 0,
-// 		name: "something"
-// 	},
-// 	{
-// 		id: 0,
-// 		name: "I'm a book"
-// 	},
-// 	{
-// 		id: 0,
-// 		name: "I'm a book"
-// 	},
-// 	{
-// 		id: 0,
-// 		name: "I'm a book"
-// 	},
-// ]
 
-// const [book1, book2, book3, book4] = bookList
+// In the background, with React, we don't write this code
+// const productListProps = {
+//     productList: [ { id: 0, title: "Shoes" } ],
+//     anotherProp: { something: 3, hello: 5 }
+// }
+// ProductList(productListProps)
 
-// export default function App() {
-// 	return (
-// 		<div className="app">
-// 			<Button text="Submit" size={2} />
-// 		</div>
-// 	)
+
+
+// function getMeal() {
+//     return ["spaghetti", "ice cream"]
 // }
 
-function Button({ size, text }) {
-	return (
-		<button className={"b-" + size}>{text}</button>
-	)
-}
 
-
-
-// constant
-// const COLORS = ["RED", "GREEN", "BLUE", "ORANGE"]
-
-
-// function makeDinner(ingredients) {
-// 	// const variable
-// 	const mixedIngredients = ingredients + " mixed"
-
-// 	if(mixedIngredients = "ricemixed") {
-
-// 	}
-// }
-
-// makeDinner("rice")
-// makeDinner("cauliflower")
-
-
-function getFood() {
-	return ["burrito", "cookies"]
-}
-
-
-const [apple, banana] = getFood()
+// const [healthyBit, goodBit] = getMeal()
--- src/productService.js
+++
@@ -1,7 +0,0 @@
-
-
-export const deleteProduct = async (id) => {
-    const response = await fetch("/products/" + id, {
-        method: "DELETE"
-    })
-}
